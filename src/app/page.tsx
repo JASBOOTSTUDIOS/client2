477e5262d644ad6b4fabe5a98c73a78c420b607f
Jose Luis Rojas Severino <jorojas@DCTIC.GC>
2025-02-14 00:35:56 -0400

    Pruevas con css grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Navbar from "@/components/navbar";
 import MainContent from "@/components/mainContent";
+import Footer from "@/components/footer";
 export default function Home() {
   return (
     <div className="">
       <Navbar />
       <MainContent/>
-      <div className="container-fluid">
-        <h1>Jefry Astacio</h1>
-      </div>
+    <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+export default function Footer(){
+    return (
+        <>
+        <div className="main-footer">
+            <h3 className="txt-main">Este es el footer</h3>
+        </div>
+        </>
+    )
+}
