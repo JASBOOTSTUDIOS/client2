968e4252f96d3f2d06f623f54c68c1664981e317
Jose Luis Rojas Severino <jorojas@DCTIC.GC>
2025-02-13 23:42:21 -0400

    Provando css Grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/navbar";
+import MainContent from "@/components/mainContent";
 export default function Home() {
   return (
     <div className="">
       <Navbar />
+      <MainContent/>
       <div className="container-fluid">
         <h1>Jefry Astacio</h1>
       </div>
---
+++ src/components/mainContent.tsx
@@ -0,0 +1,16 @@
+import "./mainContent.css"
+export default function MainContent(){
+return (<>
+<div className="mainContainer">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+</div>
+</>)
+}
